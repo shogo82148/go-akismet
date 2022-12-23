da5908ecef17c2da3702ac3773995f45ffff7adf
ICHINOSE Shogo <shogo82148@gmail.com>
2022-12-23 17:27:27 +0900

    implement submit ham and spam

--- akismet_test.go
+++ akismet_test.go
@@ -159,3 +159,75 @@ func TestCheck_Error(t *testing.T) {
 		t.Error("want some error, but not")
 	}
 }
+
+func TestSubmitHam(t *testing.T) {
+	t.Parallel()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if got, want := r.URL.Path, "/submit-ham"; got != want {
+			t.Errorf("unexpected path: got %s, want %s", got, want)
+		}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			t.Error(err)
+			return
+		}
+		if got, want := string(body), "api_key=very-secret&blog=https%3A%2F%2Fexample.com&is_test=1&user_ip=192.0.2.1&user_role=administrator"; want != got {
+			t.Errorf("got %s, want %s", got, want)
+		}
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "false")
+	}))
+	defer ts.Close()
+
+	c := &Client{
+		HTTPClient: ts.Client(),
+		BaseURL:    ts.URL,
+		APIKey:     "very-secret",
+	}
+	err := c.SubmitHam(context.Background(), &Comment{
+		Blog:     "https://example.com",
+		UserIP:   "192.0.2.1",
+		UserRole: "administrator",
+		IsTest:   true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubmitSpam(t *testing.T) {
+	t.Parallel()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if got, want := r.URL.Path, "/submit-spam"; got != want {
+			t.Errorf("unexpected path: got %s, want %s", got, want)
+		}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			t.Error(err)
+			return
+		}
+		if got, want := string(body), "api_key=very-secret&blog=https%3A%2F%2Fexample.com&is_test=1&user_ip=192.0.2.1&user_role=administrator"; want != got {
+			t.Errorf("got %s, want %s", got, want)
+		}
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "false")
+	}))
+	defer ts.Close()
+
+	c := &Client{
+		HTTPClient: ts.Client(),
+		BaseURL:    ts.URL,
+		APIKey:     "very-secret",
+	}
+	err := c.SubmitSpam(context.Background(), &Comment{
+		Blog:     "https://example.com",
+		UserIP:   "192.0.2.1",
+		UserRole: "administrator",
+		IsTest:   true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
